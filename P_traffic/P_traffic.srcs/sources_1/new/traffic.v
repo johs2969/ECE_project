26625ede2bd025101ac3e861e40c9d36f5ba30c4
johs2969 <johs2969@uos.ac.kr>
2022-11-26 15:52:13 +0900

    Update traffic.v

--- P_traffic/P_traffic.srcs/sources_1/new/traffic.v
+++ P_traffic/P_traffic.srcs/sources_1/new/traffic.v
@@ -1,42 +1,14 @@
 `timescale 1ns / 1ps
+//10khz 기준
+module traffic(rst, clk, led_red, led_yellow, led_green, led_left, led_walk_red, led_walk_green);
 
-module traffic(rst, clk, flicker, sw_flicker, led_red, led_yellow, led_green, led_left, led_walk_red, led_walk_green, led_manual_flicker );
-
-input clk,rst,flicker,sw_flicker;
-output reg [3:0] led_red, led_yellow, led_green, led_walk_red, led_walk_green;
-output [3:0] led_left;
-output [1:0] led_manual_flicker;
+input clk,rst;
+output reg [3:0] led_red, led_yellow, led_green, led_walk_red, led_walk_green, led_left;
 
 reg [13:0] cnt_1h;
-reg [3:0] cnt_circle;
-reg [1:0] direction;
+wire clk_flicker;
 
-parameter S=2'b00,
-          W=2'b01,
-          E=2'b10,
-          N=2'b11;
-            
-reg manual_flicker;
-reg [1:0] reg_sw_flicker;
-
-wire clk_1h, flicker_en, clk_flicker;
-
-always @(posedge clk or posedge rst) begin
-    if(rst)
-        reg_sw_flicker <= 2'b00;
-    else
-        reg_sw_flicker <= {reg_sw_flicker[0],sw_flicker};
-end
-
-always @(posedge clk or posedge rst) begin
-    if(rst)   
-       manual_flicker <= 0;
-    else if(reg_sw_flicker <= 2'b01)
-      manual_flicker <= ~ manual_flicker;               
-end
-
-assign led_manual_flicker = manual_flicker;
-assign flicker_en = ((manual_flicker == 1) || (flicker == 1) ? 1:0);
+// 점멸신호 10khz 기준으로 0.5초마다 flicker가 바뀌며 점멸한다.
 
 always @(posedge clk or posedge rst) begin
     if(rst) 
@@ -47,22 +19,93 @@ always @(posedge clk or posedge rst) begin
      cnt_1h <= cnt_1h +1;
 end
 
-assign clk_1h = (cnt_1h >=9999) ? 1:0;
 assign clk_flicker = (cnt_1h >=4999) ? 0:1;            
 
+// state 변경 ,aaa는 주간 시간, bbb는 야간 시간이다. 실제로 내부시계를 이용해서 구현해야하는데, 일단 임시로 설정.
+
+reg [3:0] state;
+parameter A1=4'b0000,
+          A2=4'b0001,
+          B=4'b0010,
+          C=4'b0011,
+          D=4'b0100,
+          E1=4'b0101,
+          E2=4'b0110,
+          F=4'b0111,
+          G=4'b1000,
+          H=4'b1001;
+
+integer cnt;
+reg [31:0] hold_t=0;
+
 always @(posedge clk or posedge rst) begin
-    if(rst) 
-        direction <= S;
-    else if (flicker_en == 1)
-        direction <= S;
-    else if((clk_1h ==1) && (cnt_circle ==12))
-        case(direction)
-            S :direction <= N;
-            N :direction <= W;    
-            W :direction <= E;
-            E :direction <= S;
-        endcase
+    if(rst) begin
+        state <= A1;
+        cnt <=0;
+        end
+    else if(aaa) begin// 5초 유지시간 주간 case
+        hold_t<= 50000;
+        case(state)
+            A1:begin if(cnt >= 50000) state <= D;
+            if(cnt >= 50000) cnt <=0;
+                else cnt <= cnt+1;
+            end    
+            D :begin if(cnt >= 50000) state <= E1;
+            if(cnt >= 50000) cnt <=0;
+                else cnt <= cnt+1;
+            end                 
+            E1 :begin if(cnt >= 50000) state <= F;
+            if(cnt >= 50000) cnt <=0;
+                else cnt <= cnt+1;
+            end 
+            F :begin if(cnt >= 50000) state <= E2;
+            if(cnt >= 50000) cnt <=0;
+                else cnt <= cnt+1;
+            end 
+            E2 :begin if(cnt >= 50000) state <= G;
+            if(cnt >= 50000) cnt <=0;
+                else cnt <= cnt+1;
+            end 
+            G :begin if(cnt >= 50000) state <= A1;
+            if(cnt >= 50000) cnt <=0;
+                else cnt <= cnt+1;
+            end 
+            default: state <= A1;
+       endcase
+     end  
+    else if(bbb) begin // 10초 유지시간으로 변경해준다 야간 case    
+         hold_t<= 100000;
+       case(state)
+            A1:begin if(cnt >= 100000) state <= B;
+            if(cnt >= 100000) cnt <=0;
+                else cnt <= cnt+1;
+            end    
+            B :begin if(cnt >= 100000) state <= A2;
+            if(cnt >= 100000) cnt <=0;
+                else cnt <= cnt+1;
+            end                 
+            A2 :begin if(cnt >= 100000) state <= C;
+            if(cnt >= 100000) cnt <=0;
+                else cnt <= cnt+1;
+            end 
+            C :begin if(cnt >= 100000) state <= E1;
+            if(cnt >= 100000) cnt <=0;
+                else cnt <= cnt+1;
+            end 
+            E1 :begin if(cnt >= 100000) state <= H;
+            if(cnt >= 100000) cnt <=0;
+                else cnt <= cnt+1;
+            end 
+            H :begin if(cnt >= 100000) state <= A1;
+            if(cnt >= 100000) cnt <=0;
+                else cnt <= cnt+1;
+            end 
+            default: state <= A1;
+       endcase    
+      end
 end 
+
+// state별 신호등 
 
 always @(posedge clk or posedge rst) begin
     if(rst) begin
@@ -72,111 +115,184 @@ always @(posedge clk or posedge rst) begin
         led_walk_red <=4'b0000;
         led_walk_green <=4'b0000;
         end
-    else if (flicker_en == 1) begin
-        led_red <=4'b0000;
-        led_green <=4'b0000;
-        led_yellow <={4{clk_flicker}};
-        led_walk_red <={4{clk_flicker}};
-        led_walk_green <=4'b0000;    
-        end
     else
-        case(direction)
-            S: begin
-                if(cnt_circle <=10) begin
-                    led_red <=4'b0111;
-                    led_green <=4'b1000;
-                    led_yellow <=4'b0000;    
+        case(state)// 남.북, 서,,동 순서이다.
+            A1,A2: begin
+                if(cnt <= (5/6)*hold_t) begin
+                    led_red <=4'b0011;
+                    led_green <=4'b1100;
+                    led_yellow <=4'b0000;
+                    led_left <=4'b0000;   
                   end 
                  else begin
-                    led_red <=4'b0111;
+                    led_red <=4'b0011;
                     led_green <=4'b0000;
-                    led_yellow <=4'b1000;
+                    led_yellow <=4'b1100;
+                    led_left <=4'b0000; 
                     end
-                 if(cnt_circle <=3) begin
-                    led_walk_red <=4'b1011;
-                    led_walk_green <=4'b0100;   
+                 if(cnt <= hold_t/2) begin
+                    led_walk_red <=4'b1100;
+                    led_walk_green <=4'b0011;   
                     end    
-                 else if(cnt_circle <=10) begin  
-                   led_walk_red <=4'b1011;
-                    led_walk_green <={1'b0,~clk_flicker, 2'b00}; 
-                    end
-                 else begin
-                    led_walk_red <=4'b1111;
-                    led_walk_green <=4'b0000;   
+                 else if(cnt > hold_t/2) begin  
+                   led_walk_red <=4'b1100;
+                    led_walk_green <={2'b00,~clk_flicker,~clk_flicker}; 
                     end
                  end
-             N: begin
-                if(cnt_circle <=10) begin
+             B: begin // 남.북, 서,,동 순서이다.
+                if(cnt <= (5/6)*hold_t) begin
                     led_red <=4'b1011;
                     led_green <=4'b0100;
-                    led_yellow <=4'b0000;    
+                    led_yellow <=4'b0000;
+                    led_left <=4'b0100;   
                   end 
                  else begin
                     led_red <=4'b1011;
                     led_green <=4'b0000;
                     led_yellow <=4'b0100;
+                    led_left <=4'b0000; 
                     end
-                 if(cnt_circle <=3) begin
+                 if(cnt <= hold_t/2) begin
+                    led_walk_red <=4'b1110;
+                    led_walk_green <=4'b0001;   
+                    end    
+                 else if(cnt > hold_t/2) begin  
+                   led_walk_red <=4'b1110;
+                    led_walk_green <={3'b00,~clk_flicker}; 
+                    end
+                 end
+             C: begin // 남.북, 서,,동 순서이다.
+                if(cnt <= (5/6)*hold_t) begin
+                    led_red <=4'b0111;
+                    led_green <=4'b1000;
+                    led_yellow <=4'b0000;
+                    led_left <=4'b1000;   
+                  end 
+                 else begin
+                    led_red <=4'b0111;
+                    led_green <=4'b0000;
+                    led_yellow <=4'b1000;
+                    led_left <=4'b0000; 
+                    end
+                 if(cnt <= hold_t/2) begin
                     led_walk_red <=4'b1101;
                     led_walk_green <=4'b0010;   
                     end    
-                 else if(cnt_circle <=10) begin  
+                 else if(cnt > hold_t/2) begin  
                    led_walk_red <=4'b1101;
-                    led_walk_green <={2'b00,~clk_flicker, 1'b0}; 
+                    led_walk_green <={2'b00,~clk_flicker,1'b0}; 
                     end
+                 end
+              D: begin // 남.북, 서,,동 순서이다.
+                if(cnt <= (5/6)*hold_t) begin
+                    led_red <=4'b0011;
+                    led_green <=4'b0000;
+                    led_yellow <=4'b0000;
+                    led_left <=4'b1100;   
+                  end 
                  else begin
+                    led_red <=4'b0011;
+                    led_green <=4'b0000;
+                    led_yellow <=4'b1100;
+                    led_left <=4'b0000; 
+                    end
+                 if(cnt <= hold_t/2) begin
                     led_walk_red <=4'b1111;
                     led_walk_green <=4'b0000;   
+                    end    
+                 else if(cnt > hold_t/2) begin  
+                   led_walk_red <=4'b1111;
+                    led_walk_green <=4'b0000;
                     end
-                 end                
-             W: begin
-                if(cnt_circle <=10) begin
+                 end
+              E1,E2: begin // 남.북, 서,,동 순서이다.
+                if(cnt <= (5/6)*hold_t) begin
+                    led_red <=4'b1100;
+                    led_green <=4'b0011;
+                    led_yellow <=4'b0000;
+                    led_left <=4'b0000;   
+                  end 
+                 else begin
+                    led_red <=4'b1100;
+                    led_green <=4'b0000;
+                    led_yellow <=4'b0011;
+                    led_left <=4'b0000; 
+                    end
+                 if(cnt <= hold_t/2) begin
+                    led_walk_red <=4'b0011;
+                    led_walk_green <=4'b1100;   
+                    end    
+                 else if(cnt > hold_t/2) begin  
+                   led_walk_red <=4'b0011;
+                    led_walk_green <={~clk_flicker,~clk_flicker,2'b0}; 
+                    end
+                 end
+              F: begin // 남.북, 서,,동 순서이다.
+                if(cnt <= (5/6)*hold_t) begin
                     led_red <=4'b1101;
                     led_green <=4'b0010;
-                    led_yellow <=4'b0000;    
+                    led_yellow <=4'b0000;
+                    led_left <=4'b0010;   
                   end 
                  else begin
                     led_red <=4'b1101;
                     led_green <=4'b0000;
                     led_yellow <=4'b0010;
+                    led_left <=4'b0000; 
                     end
-                 if(cnt_circle <=3) begin
-                    led_walk_red <=4'b1110;
-                    led_walk_green <=4'b0001;   
+                 if(cnt <= hold_t/2) begin
+                    led_walk_red <=4'b1011;
+                    led_walk_green <=4'b0100;   
                     end    
-                 else if(cnt_circle <=10) begin  
-                   led_walk_red <=4'b1110;
-                    led_walk_green <={3'b000,~clk_flicker}; 
-                    end
-                 else begin
-                    led_walk_red <=4'b1111;
-                    led_walk_green <=4'b0000;   
+                 else if(cnt > hold_t/2) begin  
+                   led_walk_red <=4'b1011;
+                    led_walk_green <={1'b0,~clk_flicker,2'b00};
                     end
                  end
-              E: begin
-                if(cnt_circle <=10) begin
+               G: begin // 남.북, 서,,동 순서이다.
+                if(cnt <= (5/6)*hold_t) begin
                     led_red <=4'b1110;
                     led_green <=4'b0001;
-                    led_yellow <=4'b0000;    
+                    led_yellow <=4'b0000;
+                    led_left <=4'b0001;   
                   end 
                  else begin
                     led_red <=4'b1110;
                     led_green <=4'b0000;
                     led_yellow <=4'b0001;
+                    led_left <=4'b0000; 
                     end
-                 if(cnt_circle <=3) begin
+                 if(cnt <= hold_t/2) begin
                     led_walk_red <=4'b0111;
                     led_walk_green <=4'b1000;   
                     end    
-                 else if(cnt_circle <=10) begin  
+                 else if(cnt > hold_t/2) begin  
                    led_walk_red <=4'b0111;
-                    led_walk_green <={~clk_flicker, 3'b000}; 
+                    led_walk_green <={~clk_flicker,3'b000};
                     end
+                 end 
+              H: begin // 남.북, 서,,동 순서이다.
+                if((cnt <= (5/6)*hold_t) begin
+                    led_red <=4'b1100;
+                    led_green <=4'b0000;
+                    led_yellow <=4'b0000;
+                    led_left <=4'b0011;   
+                  end 
                  else begin
+                    led_red <=4'b1100;
+                    led_green <=4'b0000;
+                    led_yellow <=4'b0011;
+                    led_left <=4'b0000; 
+                    end
+                 if(cnt <= hold_t/2) begin
                     led_walk_red <=4'b1111;
                     led_walk_green <=4'b0000;   
+                    end    
+                 else if(cnt > hold_t/2) begin  
+                   led_walk_red <=4'b1111;
+                    led_walk_green <=4'b0000;
                     end
-                 end
+                 end   
                default: begin
                     led_red <=4'b0000;
                     led_green <=4'b0000;
@@ -186,6 +302,5 @@ always @(posedge clk or posedge rst) begin
                   end
                 endcase
 end
-
-assign led_left = led_green;                     
+                    
 endmodule
