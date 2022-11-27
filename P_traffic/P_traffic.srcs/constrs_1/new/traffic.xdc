900107e1d241736c2acf0f4bcdb6506d1d7abbf7
johs2969 <johs2969@uos.ac.kr>
2022-11-27 23:43:25 +0900

    project

--- P_traffic/P_traffic.srcs/constrs_1/new/traffic.xdc
+++ P_traffic/P_traffic.srcs/constrs_1/new/traffic.xdc
@@ -77,3 +77,10 @@ set_property PACKAGE_PIN G6 [get_ports LCD_RS]
 
 set_property IOSTANDARD LVCMOS33 [get_ports break]
 set_property PACKAGE_PIN N4 [get_ports break]
+
+set_property IOSTANDARD LVCMOS33 [get_ports a]
+set_property IOSTANDARD LVCMOS33 [get_ports b]
+set_property IOSTANDARD LVCMOS33 [get_ports c]
+set_property PACKAGE_PIN Y1 [get_ports a]
+set_property PACKAGE_PIN W3 [get_ports b]
+set_property PACKAGE_PIN U2 [get_ports c]
--- P_traffic/P_traffic.srcs/sources_1/new/traffic.v
+++ P_traffic/P_traffic.srcs/sources_1/new/traffic.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 //10khz 기준
-module traffic(rst, clk, hour_up, break, led_red, led_yellow, led_green, led_left, led_walk_red, led_walk_green,LCD_E,LCD_RS,LCD_RW,LCD_DATA );
+module traffic(rst, clk, hour_up, break,a,b,c, led_red, led_yellow, led_green, led_left, led_walk_red, led_walk_green,LCD_E,LCD_RS,LCD_RW,LCD_DATA );
 
-input clk,rst,hour_up,break;
+input clk,rst,hour_up,break,a,b,c;
 output reg [3:0] led_red, led_yellow, led_green, led_walk_red, led_walk_green, led_left;
 
 reg [13:0] cnt_1h;
@@ -10,7 +10,7 @@ wire clk_flicker,hour_up_t,break_t;
 reg [7:0] hour,min,sec;
 integer cccnt;
 
-reg [3:0] state;
+reg [3:0] state,state3;
 parameter A1=4'b0000,
           A2=4'b0001,
           B=4'b0010,
@@ -25,6 +25,7 @@ parameter A1=4'b0000,
 oneshot_universal #(.width(2)) uut(.clk(clk), .rst(rst), .btn({hour_up, break}), .btn_trig({hour_up_t,break_t}));
  //시간 계산 10khz기준 시간이 100배 빠르게 흐를것임. 대략 14분이면 하루가 돌게 된다.
  
+ integer d=99;
 always @(posedge clk or posedge rst) begin   
         if(rst) begin
         cccnt <=0;
@@ -33,7 +34,9 @@ always @(posedge clk or posedge rst) begin
         min <=0;
         end
     else if(hour_up_t) hour <= hour+1;// 버튼 누르면 강제로 1시간 증가. 원샷트리거로 작동.
-        else if (cccnt < 99)  cccnt <= cccnt+1; //1clk마다 cccnt 증가, 실제1초가 내부시계0.01초인 셈. 
+    else if (cccnt < d)  begin cccnt <= cccnt+1; //1clk마다 cccnt 증가, 실제1초가 내부시계0.01초인 셈. 실제시간의 100배
+            d <= a? 9999 :(b? 999 : (c? 49: 99)); //a=실제시간의 1배, b= 실제시간의 10배, c=실제시간의 200배 dip 스위치를 사용할것이니, 꾹 누르지 않아도 됨(배율 유지가능)
+            end
        else begin //cnt==99. 99밀리초->0밀리초 넘어갈 때
            cccnt <= 0;
          if (sec < 59) sec <= sec+1; //99+1밀리초마다 초 단위 증가.
@@ -49,7 +52,7 @@ always @(posedge clk or posedge rst) begin
          end //min==59
       end //sec==59                   
   end //cnt==99
-end
+end         
 
 //text LCD
 wire [7:0] bcd1,bcd2,bcd3;
@@ -234,34 +237,33 @@ begin
                             H: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0100_1000; // H
                         endcase
                         end                  
-                    10 : {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0010_0000; //
-                    11 : {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0010_1000; // (                   
-                    12 :begin case(hour) //시간으로 주/야간 판단
+                    10 : {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0010_1000; // (                   
+                    11 :begin case(hour) //시간으로 주/야간 판단
                             8,9,10,11,12,13,14,15,16,17,18,19,20,21,22: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0010_0000; //
                             23,0,1,2,3,4,5,6,7: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0110_1110; // n
                         endcase
                         end
-                    13 :begin case(hour) //시간으로 주/야간 판단
+                    12 :begin case(hour) //시간으로 주/야간 판단
                             8,9,10,11,12,13,14,15,16,17,18,19,20,21,22: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0110_0100; // d
                             23,0,1,2,3,4,5,6,7: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0110_1001; // i
                         endcase
                         end
-                     14 :begin case(hour) //시간으로 주/야간 판단
+                     13 :begin case(hour) //시간으로 주/야간 판단
                             8,9,10,11,12,13,14,15,16,17,18,19,20,21,22: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0110_0001; // a
                             23,0,1,2,3,4,5,6,7: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0110_0111; // g
                         endcase
                         end     
-                     15 :begin case(hour) //시간으로 주/야간 판단
+                     14 :begin case(hour) //시간으로 주/야간 판단
                             8,9,10,11,12,13,14,15,16,17,18,19,20,21,22: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0111_1001; // y
                             23,0,1,2,3,4,5,6,7: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0110_1000; // h
                         endcase
                         end    
-                     16 :begin case(hour) //시간으로 주/야간 판단
+                     15 :begin case(hour) //시간으로 주/야간 판단
                             8,9,10,11,12,13,14,15,16,17,18,19,20,21,22: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0010_0000; //
                             23,0,1,2,3,4,5,6,7: {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0111_0100; // t
                         endcase
                         end 
-                     17 : {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0010_1001; // )                         
+                     16 : {LCD_RS, LCD_RW, LCD_DATA} <= 10'b1_0_0010_1001; // )                         
                     default : {LCD_RS, LCD_RW, LCD_DATA} <=10'b1_0_0010_0000; // 
                  endcase
               end
@@ -303,12 +305,15 @@ always @(posedge clk or posedge rst) begin
     else if(break_t || x) begin // 신호등 수동조작, 긴급차량 발생, 
         hold_t<= 150000;
         if(cnt >= 10000 && x==1) state <= A1; // 1초 흐르고 state A로 전환
-        if(cnt >= 160000) cnt <=0;
+        if(cnt >= 160000) begin cnt <=0;
+        state <= state3;
+        end
             else cnt <= (x==0)? 0: cnt+1; // cnt 가 처음에 들어올때 0으로 초기화 되어야함. 
         x <= (cnt >= 160000) ? 0:1;  // 처음에 break_t신호가 들어오면 x=1이 되면서 수동조작 상태가 유지된다. 그러다가 cnt=150000 15초가 지나면, x=0이되면서 수동조작 상태가 꺼지고 주,야간으로 되돌아간다.
         end
     else if(hour >= 8 && hour < 23) begin// 5초 유지시간 주간 case
         hold_t<= 50000;
+        state3 <=state;
         case(state)
             A1:begin if(cnt >= 50000) state <= D;
             if(cnt >= 50000) cnt <=0;
@@ -339,6 +344,7 @@ always @(posedge clk or posedge rst) begin
      end  
     else if(hour < 8 || hour == 23) begin // 10초 유지시간으로 변경해준다 야간 case    
          hold_t<= 100000;
+         state3 <=state;
        case(state)
             A1:begin if(cnt >= 100000) state <= B;
             if(cnt >= 100000) cnt <=0;
